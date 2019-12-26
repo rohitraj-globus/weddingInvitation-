bfacd4f878c22d15d0fc9a9877d63c51c43afb1e
ROHIT RAJ <rohitraj-globus@users.noreply.github.com>
2019-12-27 04:14:06 +0530

    Add files via upload

---
+++ public/js/parallax.js
@@ -0,0 +1,188 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDabsnLsmIYbJI3TSwV7AELUWiEvW8-KUI",
+  authDomain: "weddinginvitationlist-ba3f1.firebaseapp.com",
+  databaseURL: "https://weddinginvitationlist-ba3f1.firebaseio.com",
+  projectId: "weddinginvitationlist-ba3f1",
+  storageBucket: "weddinginvitationlist-ba3f1.appspot.com",
+  messagingSenderId: "666444388099",
+  appId: "1:666444388099:web:e2f0d8d7c43655dd5d7b36",
+  measurementId: "G-87X74L29SN"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+/* snackbar & fireBase */
+var messageRef = firebase.database().ref('messages');
+function submitForm() {
+  var name = $('#name').val();
+  var contact = $('#contactNo').val();
+  var email = $('#emailId').val();
+  var message = $('#message').val();
+  var guest = $( "#guest" ).val();
+  console.log(name+"/"+contact+"/"+email+"/"+message+"/"+guest);
+  saveMessages(name,contact,email,message,guest);
+
+  var x = document.getElementById("snackbar");
+  x.className = "show";
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+
+  $('#name').val(null); $('#contactNo').val(' '); $('#emailId').val(' '); $('#message').val(null); $( "#guest" ).val('1');
+}
+
+function saveMessages(name,contact,email,message,guest){
+  var newMessage = messageRef.push();
+  newMessage.set({
+    name:name,
+    contact:contact,
+    email:email,
+    message:message,
+    guest:guest
+  })
+}
+/* snackbar & fireBase */
+
+$(window).resize(function(){
+  section1DivResize();
+});
+
+$(window).load(function(){
+  section1DivResize();
+
+  $(".heartBackground").animate({
+    height: "150",
+    width: "150",
+    opacity: 1
+  },'5000','linear');
+});
+
+
+
+$(window).scroll(function() {
+  var heights = $(window).width()/2.1 - 49; 
+  if( $(window).scrollTop() >= heights ){
+    $("#rr_navigation").css({"top": 0 }) ;
+    $("#rr_navigation").css({"position": 'fixed' }) ;
+  }else{
+    section1DivResize();
+    $("#rr_navigation").css({"position": 'absolute' }) ;
+  }
+  parallax();
+});
+
+function section1DivResize() {
+  //console.log($(window).width());
+  var heights = $(window).width()/2.1 ; 
+  //console.log(heights);
+    $(".rr_staticParallax1").css({"height": heights }) ;
+    $(".rr_staticParallax2").css({"height": heights }) ;
+    //show hide mobile/desktop header
+    if($(window).width()<675){
+      $(".rr_menuContainer").removeClass("hide");
+      $("#rr_navigation").addClass("hide");
+      $(".familyMemberPic").attr("width","100");
+      $(".familyMemberPic").attr("height","100");
+    }else{
+      $(".rr_menuContainer").addClass("hide");
+      $("#rr_navigation").removeClass("hide");
+      $("#rr_navigation").css({"top": (heights-50) }) ;
+      $(".familyMemberPic").attr("width","150");
+      $(".familyMemberPic").attr("height","150");
+    }
+}
+
+function toggleMenu() {
+  $(".menu").toggleClass("open");
+  $(".rr_mobileMenu").toggleClass("hide");
+  /*var menu = document.querySelector('.menu');
+        if(menu.classList.contains("open")){component.set("v.openMobileNavMenu",false);}
+        else{ component.set("v.openMobileNavMenu",true); }
+        menu.classList.toggle('open');*/
+}
+
+/* Image Carousel */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+var interval = setInterval(function(){ 
+  slideIndex++;
+  showSlides(slideIndex);
+  //console.log("test") ;
+}, 7000);  // Change image every 7 seconds
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+  //clearInterval(interval);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+/* Image Carousel */
+
+/*  digital clock */
+var clock;
+$(document).ready(function() {
+  // Grab the current date
+  var currentDate = new Date();
+
+    // Set some date in the future. In this case, it's always Jan 1
+    var futureDate  = new Date(2020,00,18,18,16,0,0);
+
+    // Calculate the difference in seconds between the future and current date
+    var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
+
+  // Instantiate a coutdown FlipClock
+  clock = $('.clock').FlipClock(diff, {
+    clockFace: 'DailyCounter',
+    countdown: true
+  });
+});
+/*  digital clock */
+
+function parallax() {
+var wScroll = $(window).scrollTop();
+//console.log("wScroll-"+wScroll)
+  $('.rr_parallax--bg').css('background-position', 'center ' + (wScroll*0.96)+'px');
+}
+
+function openCurtainEffect(family) {
+  $(".cover-left").css("left","-50%");
+  $(".cover-right").css("right","-50%");
+  if(family==="bridesmaides"){
+    $(".bridesmaidesFamily").removeClass("hide");
+    //$("#groomsmen").addClass("hide");
+  }else{
+    $(".groomsmenFamily").removeClass("hide");
+    //$("#rr_navigation").addClass("hide");
+  }
+}
+
+function closeFamily(family) {
+  $(".cover-left").css("left","0px");
+  $(".cover-right").css("right","0px");
+  if(family==="bridesmaides"){
+    $(".bridesmaidesFamily").addClass("hide");
+  }else{
+    $(".groomsmenFamily").addClass("hide");
+  }
+}
+
+setTimeout(function(){ window.scrollTo(0, 0); }, 700);
